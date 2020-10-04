cfedcf5127d713efda5ee00302d86387603bfd65
fortunetede <fortunetede@gmail.com>
2020-10-04 21:35:29 +0000

    init push with login and logout test_cases

---
+++ cases/tests/login.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+import time
+import unittest
+import HtmlTestRunner
+
+import sys
+sys.path.insert(1, '../../cases')
+
+from pages.loginPage import LoginPage
+from pages.homePage import HomePage
+
+class LoginTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path='../../drivers/chromedriver')
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
+        
+
+    def test_login_valid(self):
+        driver = self.driver
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+
+        login = LoginPage(driver)
+        login.enter_username("Admin")
+        login.enter_password("admin123")
+        login.click_login()  
+
+        logout = HomePage(driver)
+        logout.click_welcome()
+        logout.click_logout()
+        
+    @classmethod
+    def tearDownClass(cls):
+        time.sleep(4)
+        cls.driver.close()
+        cls.driver.quit()
+        # print("Test Completed")
+
+
+if __name__ == '__main__':
+    unittest.main(
+        testRunner=HtmlTestRunner.HTMLTestRunner(output="../../cases/report")
+    )
+
+
