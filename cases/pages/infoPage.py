00eb9cb39e98c949e0d6f0df3453285aa5d56064
fortunetede <fortunetede@gmail.com>
2020-10-06 13:06:54 +0000

    validate user infor

--- cases/pages/infoPage.py
+++ cases/pages/infoPage.py
@@ -1,5 +1,5 @@
 import time
-
+import logging
 import sys
 sys.path.insert(2, '../../cases')
 sys.path.insert(3, '../pages')
@@ -18,27 +18,43 @@ class InfoPage(LoginPage, UtilsFunctions):
 
 
     def update_personal_detail(self):
+        log = logging.getLogger("TestLog")
+        log.debug("debug message")
+
+        driver = self.driver
         time.sleep(2)
-        self.driver.find_element_by_id(Locators.my_info_link_id).click()
+        
+        driver.find_element_by_id(Locators.my_info_link_id).click()
         time.sleep(1)
-        self.driver.find_element_by_id(Locators.save_button_id).click()
+        driver.find_element_by_id(Locators.save_button_id).click()
         time.sleep(1)
-        self.driver.find_element_by_id(Locators.personal_firstname_textbox_id).clear()
-        self.driver.find_element_by_id(Locators.personal_firstname_textbox_id).send_keys("Fortune")
+        driver.find_element_by_id(Locators.personal_firstname_textbox_id).clear()
+        driver.find_element_by_id(Locators.personal_firstname_textbox_id).send_keys("Fortune")
         time.sleep(1)
-        self.driver.find_element_by_id(Locators.personal_middlename_textbox_id).clear()
-        self.driver.find_element_by_id(Locators.personal_middlename_textbox_id).send_keys("Mensah")
+        driver.find_element_by_id(Locators.personal_middlename_textbox_id).clear()
+        driver.find_element_by_id(Locators.personal_middlename_textbox_id).send_keys("Mensah")
         time.sleep(1)
-        self.driver.find_element_by_id(Locators.personal_lastname_textbox_id).clear()
-        self.driver.find_element_by_id(Locators.personal_lastname_textbox_id).send_keys("Tede")
+        driver.find_element_by_id(Locators.personal_lastname_textbox_id).clear()
+        driver.find_element_by_id(Locators.personal_lastname_textbox_id).send_keys("Tede")
         time.sleep(1)
-        self.driver.find_element_by_id(Locators.personal_licexpdate_textbox_id).clear()
-        self.driver.find_element_by_id(Locators.personal_licexpdate_textbox_id).send_keys("2025-10-16")
+        driver.find_element_by_id(Locators.personal_licexpdate_textbox_id).clear()
+        driver.find_element_by_id(Locators.personal_licexpdate_textbox_id).send_keys("2025-10-16")
         time.sleep(1)
-        select_element = Select(self.driver.find_element_by_id(Locators.personal_marital_selectbox_id))
-        select_element.select_by_value("Single")
+        select_element = Select(driver.find_element_by_id(Locators.personal_marital_selectbox_id)).select_by_value("Single")
         time.sleep(1)
-        self.driver.find_element_by_id(Locators.save_button_id).click()
+        driver.find_element_by_id(Locators.save_button_id).click()
+
+        time.sleep(1)
+        driver.back()
+        time.sleep(1)
+        driver.forward()
+
+        
+        
+        
+
+        
+        
 
 
         
--- cases/tests/test.py
+++ cases/tests/test.py
@@ -2,13 +2,15 @@ from selenium import webdriver
 import time
 import unittest
 import HtmlTestRunner
-
+import logging
 import sys
 sys.path.insert(1, '../../cases')
 
 from pages.loginPage import LoginPage
 from pages.homePage import HomePage
 from pages.infoPage import InfoPage
+
+from utils.locators import Locators
 
 class AllFunctionsTestCases(unittest.TestCase):
 
@@ -35,7 +37,15 @@ class AllFunctionsTestCases(unittest.TestCase):
         infoPage.login("Admin", "admin123") 
         # update user 
         infoPage.update_personal_detail()
-        driver.back()
+        # # validate user update
+        check_usrname_update = driver.find_element_by_id(Locators.personal_firstname_textbox_id).get_attribute('value') 
+        self.assertEqual(check_usrname_update, "Fortune") # validate first name
+        check_usrname_update = driver.find_element_by_id(Locators.personal_middlename_textbox_id).get_attribute('value') 
+        self.assertEqual(check_usrname_update, "Mensah") # validate middle name
+        check_usrname_update = driver.find_element_by_id(Locators.personal_lastname_textbox_id).get_attribute('value') 
+        self.assertEqual(check_usrname_update, "Tede") # validate last name
+        check_usrname_update = driver.find_element_by_id(Locators.personal_licexpdate_textbox_id).get_attribute('value') 
+        self.assertEqual(check_usrname_update, "2025-10-16") # validate licexpdate
         # logout 
         infoPage.logout()
 
@@ -45,6 +55,8 @@ class AllFunctionsTestCases(unittest.TestCase):
         self.driver.close()
         self.driver.quit()
 
+
+# logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 if __name__ == '__main__':
     unittest.main(
